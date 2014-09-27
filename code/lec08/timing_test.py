d9b1f70522ee27ec5c41288813d0d284239c389e
Dustin Tran <dustinviettran@gmail.com>
2014-09-27 16:51:07 -0400

    add lec08

---
+++ code/lec08/timing_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+from time import clock,time
+import numpy as np
+import scipy.linalg
+
+# Get the current time() (a measure of "wall clock time", the physical passage of
+# time as you would observe by looking at a wall clock)
+t0=time()
+
+# Get the current clock() (a measure of the time that this program has actually
+# spent on the processor)
+c0=clock()
+
+# Carry out a large, arbitrary calculation
+k=0
+for i in range(1000):
+	for j in range(1000):
+		k+=i+j
+
+# Print the elapsed time, using both the time() routine and the clock() routine
+print "time routine:",time()-t0
+print "clock routine:",clock()-c0
